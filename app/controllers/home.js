32af6abb031617d5518c55ab59a8aa911d4b54d0
einarstensson <einar.stensson@gmail.com>
2016-06-16 14:34:14 -0400

    Add an adapter for answers

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,9 +6,19 @@ export default Ember.Controller.extend({
   },
   actions: {
     submit(newAnswer){
-      this.get('model').get('answers').addObject(this.store.createRecord('answer', {
-          content: newAnswer
-      }));
+      var question = this.get('model');
+      var answer = this.store.createRecord('answer', {
+        content: newAnswer
+      });
+
+      question.get('answers').addObject(answer);
+      answer.save();
+      
+//      this.get('model').get('answers').addObject(this.store.createRecord('answer', {
+  //      content: newAnswer
+    //  }));
+
+  //    this.get('model').save();
 
       this.set('isAnswering', false);
     }
