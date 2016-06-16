b729fa9824a0d7f1dc1b7dce8d28042e1275b250
einarstensson <einar.stensson@gmail.com>
2016-06-16 09:54:57 -0400

    Tried creating a template for individual questions with their answers

--- app/router.js
+++ app/router.js
@@ -6,7 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('home');
+  this.route('home', function() {
+    this.route('question', {
+      path: ':id'
+    });
+  });
 });
 
 export default Router;
