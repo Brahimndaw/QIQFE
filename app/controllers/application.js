d8687d54b563392569987836d08e8e29fe238f46
einarstensson <einar.stensson@gmail.com>
2016-06-17 13:02:13 -0400

    bug fixes on ajax

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,28 +6,37 @@ export default Ember.Controller.extend({
   },
   actions: {
     submit(newAnswer){
+      let question = this.get('currentQ')
+      // question.save()
+      let answer = this.store.createRecord('answer', {question: question, content: newAnswer})
+      answer.save()
+      this.set('isAnswering', false);
+      this.set('userAnswer', answer.get('content'));
+      this.set('answers', question.get('answers').content);
 
-      var question = this.get('currentQ');
-
-      var answer = { attributes:
-        { content: newAnswer,
-        question_id: question.data.id }
-      }
-
-      question.included.addObject(answer);
-      this.set('answers', question.included);
-
-      const requestOptions = {
-        url: "http://localhost:3000/api/v1/answers",
-        type: 'POST',
-        data: JSON.stringify(answer),
-        contentType: 'application/json',
-        dataType: 'json'
-      };
-
-      $.ajax(requestOptions).then((response) => {
-        this.set('isAnswering', false);
-      })
+  //     var question = this.get('currentQ');
+  //
+  //     var answer = { attributes:
+  //       { content: newAnswer,
+  //       question_id: question.data.id }
+  //     }
+  //
+  // //    question.included.addObject(answer);
+  //     this.set('userAnswer', answer.attributes.content);
+  //
+  //     this.set('answers', question.included);
+  //
+  //     const requestOptions = {
+  //       url: "http://localhost:3000/api/v1/answers",
+  //       type: 'POST',
+  //       data: JSON.stringify(answer),
+  //       contentType: 'application/json',
+  //       dataType: 'json'
+  //     };
+  //
+  //     $.ajax(requestOptions).then((response) => {
+  //       this.set('isAnswering', false);
+  //     })
     },
     getQuestion(){
       const requestOptions = {
@@ -38,8 +47,15 @@ export default Ember.Controller.extend({
       };
 
      $.ajax(requestOptions).then((response) => {
-        var currentQ = response
+
+  //      var answers = response.included.map(function(item) {
+  //        return this.store.createRecord('answer', { content: item.content });
+  //      });
+        var currentQ = this.store.createRecord('question', { id: response.data.id ,
+        content: response.data.attributes.content /*, answers: answers */ })
+        debugger
         this.set('currentQ', currentQ);
+        this.set('question', currentQ.get('content'))
         this.set('isAnswering', true);
       })
     }
