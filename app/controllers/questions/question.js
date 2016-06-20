a8f6b4908c7db2e10f7ce9ebecc9edc560bc7f38
Lamine Ndaw <brahimndaw@gmail.com>
2016-06-20 13:16:10 -0400

    up & down vote

--- app/controllers/questions/question.js
+++ app/controllers/questions/question.js
@@ -4,11 +4,24 @@ export default Ember.Controller.extend({
   isAnswering: true,
   actions: {
     submit(newAnswer){
-      debugger;
-      let answer = this.store.createRecord('answer', {content: newAnswer})
+      let answer = this.store.createRecord('answer', {content: newAnswer, voteCount: 0, voteScore: 0})
       answer.set('question', this.get('model'))
       answer.save()
       this.toggleProperty('isAnswering')
+      this.set('userAnswer', answer)
+      let arrayLength =  this.get('model').get('answers').content.length
+      let modelFetch =  this.get('model').get('answers').content.slice(0, arrayLength -1)
+      this.set('answers', modelFetch)  
+    },
+    upVote(answer){
+      answer.incrementProperty('voteCount')
+      answer.incrementProperty('voteScore')
+      answer.save();
+    }, 
+    downVote(answer) {
+      answer.incrementProperty('voteCount')
+      answer.decrementProperty('voteScore')
+      answer.save();
     }
   }
 })
